a173d5683138e006c047d132232235e076d74cf7
WickerBottomJeans <vuhaiduong24062002@gmail.com>
2025-01-13 17:44:29 +0700

    Tweak a lil bit in Enemy script

--- Assets/_Scripts/DetectorForEnemy.cs
+++ Assets/_Scripts/DetectorForEnemy.cs
@@ -9,13 +9,13 @@ public class DetectorForEnemy : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Turret")) { // Assuming turrets are tagged as "Turret"
+        if (other.CompareTag("Turret") || other.CompareTag("SpelledEnemy")) { // Assuming turrets are tagged as "Turret"
             parentEnemy?.OnTurretEnterRange(other.gameObject);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("Turret")) {
+        if (other.CompareTag("Turret") || other.CompareTag("SpelledEnemy")) {
             parentEnemy?.OnTurretExitRange(other.gameObject);
         }
     }
