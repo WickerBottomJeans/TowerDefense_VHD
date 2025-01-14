d8f8623d4757d9ccdf6744bb668215fe5707d30e
WickerBottomJeans <vuhaiduong24062002@gmail.com>
2025-01-14 10:15:34 +0700

    Tweaking fun spell

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
+    private const string LEVEL_SELECTION_SCENE = "LevelSelection";
+    private const string CHALLENGE_SCENE = "Challenge";
 
     public void PlayGame() {
-        SceneManager.LoadScene("LevelSelection");
+        SceneManager.LoadScene(LEVEL_SELECTION_SCENE);
     }
-    public void Challenge ()
-    {
-        SceneManager.LoadScene("Challenge");
-    }    
+
+    public void Challenge() {
+        SceneManager.LoadScene(CHALLENGE_SCENE);
+    }
 
     public void ExitGame() {
         Application.Quit();
